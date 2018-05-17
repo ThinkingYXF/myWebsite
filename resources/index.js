5fbc33009e2b7da02301f134f773078cafd561f6
thinkingYXF <thinking.yxf@gmail.com>
2018-05-17 15:06:18 +0800

    add fonts

--- resources/index.js
+++ resources/index.js
@@ -5,27 +5,6 @@ $(document).ready(function(){
 	$('.headerNav > li').mouseleave(function(){
 		$(this).find('ul').hide();
 	});
-	var bannerIndex = 0;
-	{
-		setInterval(function(){
-			bannerIndex++;
-			if(bannerIndex > 2)
-				bannerIndex = 0;
-			$('.bannerNav li').removeClass('native');
-			$('.bannerNav li').eq(bannerIndex).addClass('native');
-			$('.banner img').removeClass('native');
-			$('.banner img').eq(bannerIndex).addClass('native');
-		},5000);
-	}
-	$('.bannerNav li').click(function(){
-		$('.bannerNav li').removeClass('native');
-		$(this).addClass('native');
-		var index = $(this).index();
-		bannerIndex = index;
-		$('.banner img').removeClass('native');
-		$('.banner img').eq(index).addClass('native');
-	});
-
 	$('.myHobby img').click(function(){
 		var className = $(this).prop('class');
 		$.modal(className,{},function(modal){
