844fb8a086efbcbc0675b6a9b62c84887a42b27e
yangxuefeng <1070594374@qq.com>
2018-05-20 22:12:36 +0800

    mysql config

--- demo.js
+++ demo.js
@@ -1,9 +1,23 @@
 let http = require('http'),
+    config = require('config'),
+    bodyParser = require('body-parser'),
     express = require('express'),
+    mysql = require('mysql'),
     app = express();
 app.use('/', express.static(__dirname));
 var server = http.createServer(app);
+var dbconfig = config.get('dbconfig');
+var connection = mysql.createConnection(dbconfig);
+console.log(dbconfig);
+connection.connect(function(err){
+    console.log('mysql connect success');
+});
+connection.query('select user from user;',function(err, results){
+    console.log(err,results);
+});
+connection.end();
 server.listen(8086);
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.get('/me', function(req, res){
     var json = {
         success: true,
@@ -18,3 +32,11 @@ app.get('/me', function(req, res){
     res.status(200),
     res.json(json);
 });
+app.post('/send', urlencodedParser, function(req, res){
+    console.log(req.body);
+    res.status(200);
+    res.json({
+        success: true,
+        data: req.body
+    });
+})
