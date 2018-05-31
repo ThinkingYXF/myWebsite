73bd25617e586ced8857d86ab37ad60d5ae39cec
thinkingYXF <thinking.yxf@gmail.com>
2018-05-31 19:03:39 +0800

    header upload

--- interface/chat.js
+++ interface/chat.js
@@ -1,10 +1,14 @@
+const host = "47.93.232.133:8080/";
+
 var base = require('../base');
 var bodyParser = require('body-parser'),							//解析params
 	urlencodedParser = bodyParser.urlencoded({ extended: false });	//
 
+var fs = require('fs');
+
 var multer  = require('multer');
-// var upload = multer({dest: 'uploads/images'}).single('userIcon');
-var upload = multer({dest: '/tmp/images'}).single('userIcon');
+var upload = multer({dest: 'uploads/images'}).single('userIcon');
+// var upload = multer({dest: '/tmp/images'}).single('userIcon');
 exports.handler = function(connection, app){
 	//获取用户列表
 	app.get('/chatList', function(req, res){
@@ -20,22 +24,17 @@ exports.handler = function(connection, app){
 
 	//上传/更改 头像
 	app.post('/upload',upload, function(req, res){
+		var file = req.file;
 		console.log(req.file);
+		fs.renameSync('./uploads/images/'+file.filename,'./uploads/images/'+file.originalname);
 		var userName = req.session.user;
 		if(!userName) throw 'session expired';
-		res.render('websocket');
-		res.end();
 
-
-		// connection.query(base.updateImgUrl(req.body.imgUrl, userName), function(err){
-		// 	if(err) throw err;
-		// 	res.status(200);
-		// 	res.json({
-		// 		success: true,
-		// 		fileInfo: {
-		// 			url: req.body.imgUrl
-		// 		}
-		// 	});
-		// });
+		var url = host + file.destination + '/' + file.originalname;
+		connection.query(base.updateImgUrl(url, userName), function(err){
+			if(err) throw err;
+			res.status(200);
+			res.end();
+		});
 	});
 }
